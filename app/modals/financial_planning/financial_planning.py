8c7a85eb7199b559fb70419d9b38ad7290066f0d
gilbert-ku <gilbert45ku@gmail.com>
2024-03-09 03:45:19 +0300

    add financial planning table

---
+++ app/modals/financial_planning/financial_planning.py
@@ -0,0 +1,12 @@
+from app import db
+
+class FinancialPlanning(db.Model):
+    __tablename__ = "financialplanning"
+
+    id = db.Column(db.Integer, primary_key=True)
+    full_name = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False)
+    employment = db.Column(db.String, nullable=False)
+    app_date = db.Column(db.Date, nullable=False)  
+    app_time = db.Column(db.Time, nullable=False)  
+    comment = db.Column(db.String, nullable=False)  
