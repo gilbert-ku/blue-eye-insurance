e2e671c95664c1dc1cf2d43c7e89af55e0fe1cca
gilbert-ku <gilbert45ku@gmail.com>
2024-03-09 12:27:11 +0300

    add cooprate  model

--- app/modals/cooprate/cooprate.py
+++ app/modals/cooprate/cooprate.py
@@ -1,16 +1,13 @@
 from app import db
 
-class Motor(db.Model):
-    __tablename__ = 'motors'
+class Cooprate(db.Model):
+    __tablename__ = "coopretes"
 
     id = db.Column(db.Integer, primary_key=True)
     full_name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), nullable=False)
     phone = db.Column(db.integer(15), nullable=False)
-    category = db.Column(db.String(10), nullable=False)
     cover_type = db.Column(db.String(100), nullable=False)
-    year_manufc = db.Column(db.integer(10), nullable=False)
-    valuation = db.Column(db.integer(50))
     meeting = db.Column(db.String(100), nullable=False )
     app_date = db.Column(db.Date, nullable=False)
     app_time = db.Column(db.Time, nullable=False)
