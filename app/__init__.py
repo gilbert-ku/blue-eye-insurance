4ccbb5146b16d52ddb7e4be6f58f8f093a5250fa
gilbert-ku <gilbert45ku@gmail.com>
2024-03-09 03:14:42 +0300

    add restfull migrate

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from app.config.development import DevelopmentConfig
 
 app = Flask(__name__)
 app.config.from_object(DevelopmentConfig)
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 from app.routes.financial_planning.financial_planning import finacial_blueprint
 # register blueprint
--- app/routes/financial_planning/financial_planning.py
+++ app/routes/financial_planning/financial_planning.py
@@ -1,9 +1,18 @@
 from flask import Blueprint
 from flask_restful import Resource, Api
 
+
+
+
 finacial_blueprint = Blueprint("financial_planning", __name__)
 
 api = Api(finacial_blueprint)
+
+class Welcome(Resource):
+    def get(self):
+        return{"BEIA": "your one stop insurance provider"}
+    
+api.add_resource(Welcome, "/")
 
 class Finacialplanning(Resource):
     def get(self):
