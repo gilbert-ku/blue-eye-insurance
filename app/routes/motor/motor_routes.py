1df3e71c0b2f52603da8c9735690f53c34dab044
gilbert-ku <gilbert45ku@gmail.com>
2024-03-12 16:26:31 +0300

    add moto  parser request fun and route fun

--- app/routes/motor/motor_routes.py
+++ app/routes/motor/motor_routes.py
@@ -15,4 +15,31 @@ class MotorApi(Resource):
 
 api.add_resource(MotorApi, "/")
 
+# request parser
 
+parser = reqparse.RequestParser()
+parser.add_argument("full_name", type=str, help="Client's Name")
+parser.add_argument("email", type=str, help="Client's email")
+parser.add_argument("phone", type=str, help="Client's phone")
+parser.add_argument("category", type=str, help="category of insurance name")
+parser.add_argument("cover_type", type=str, help="type of cover")
+parser.add_argument("year_manufc", type=str, help="motor year of manufac")
+parser.add_argument("valuation", type=str, help="Client's car valuation")
+parser.add_argument("meeting", type=str, help="Client's meeting prefrance")
+parser.add_argument("app_date", type=str, help="Client's appointment date prefrance")
+parser.add_argument("app_time", type=str, help="Client's app time prefrance")
+parser.add_argument("comment", type=str, help="Client's app comments")
+
+class MotorAppointment(Resource):
+    def get(self):
+        pass
+    
+    def post(self):
+        
+        pass
+
+    def delete(self):
+        pass
+
+
+api.add_resource(MotorAppointment, "/motorAppointment", "/motorAppointment/<int:id>")
