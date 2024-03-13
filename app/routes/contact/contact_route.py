a49a615574020507ec8178b5b98a4d8b0a226d91
gilbert-ku <gilbert45ku@gmail.com>
2024-03-13 11:59:14 +0300

    add request parser

--- app/routes/contact/contact_route.py
+++ app/routes/contact/contact_route.py
@@ -14,3 +14,10 @@ class ContactApi(Resource):
         return{"Contact Us Appointment Api": "Welcome to Contact Us Api, This route is responsible in handling Contact Us appointment"}
     
 api.add_resource(ContactApi, "/")
+
+# request parser
+parser = reqparse.RequestParser()
+parser.add_argument("full_name", type=str, help="Client's name")
+parser.add_argument("email", type=str, help="Client's email")
+parser.add_argument("phone", type=str, help="Client's phone")
+parser.add_argument("comment", type=str, help="Client's comment")
