ac91bd3b62c4484d24235e0ca48322010019fa1e
gilbert-ku <gilbert45ku@gmail.com>
2024-03-13 10:11:16 +0300

    add parser args and aoi fun  corporate

--- app/__init__.py
+++ app/__init__.py
@@ -12,10 +12,10 @@ from app.routes.financial_planning.financial_planning import finacial_blueprint
 from app.routes.investment.investment_routing import investment_blueprint
 from app.routes.motor.motor_routes import motor_blueprint
 from app.routes.medical.medical_route import medical_blueprint
-from app.routes.cooprate.cooprate_route import cooprate_blueprint
+from app.routes.cooprate.cooprate_route import corporate_blueprint
 # register blueprint
 app.register_blueprint(finacial_blueprint)
 app.register_blueprint(investment_blueprint)
 app.register_blueprint(motor_blueprint)
 app.register_blueprint(medical_blueprint)
-app.register_blueprint(cooprate_blueprint)
+app.register_blueprint(corporate_blueprint)
--- app/routes/cooprate/cooprate_route.py
+++ app/routes/cooprate/cooprate_route.py
@@ -14,3 +14,26 @@ class CorporateApi(Resource):
         return{"Corporate Appointment Api": "Welcome to corporate Api, This route is responsible in handling corporate appointment"}
     
 api.add_resource(CorporateApi, "/")
+
+# request parser
+
+parser = reqparse.RequestParser()
+parser.add_argument("full_name", type=str, help="Client's name")
+parser.add_argument("company_name", type=str, help="Company name")
+parser.add_argument("email", type=str, help="Client's email")
+parser.add_argument("phone", type=str, help="Client's phone")
+parser.add_argument("cover_type", type=str, help="Client's cover type")
+parser.add_argument("meeting", type=str, help="Client's meeting prefrance")
+parser.add_argument("app_date", type=str, help="Client's appointment date")
+parser.add_argument("app_time", type=str, help="Client's appointment time")
+parser.add_argument("comment", type=str, help="Client's comment")
+
+class CorporateAppointment(Resource):
+    def get(self):
+        pass
+    def post(self):
+        pass
+    def delete(self):
+        pass
+
+api.add_resource(CorporateAppointment, "/corporateAppointment", "/corporateAppointment/<int:id>")
